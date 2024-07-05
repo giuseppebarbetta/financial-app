ac445971321f9b31bc3d61eaf68bcf448a7fc885
giuseppebarbetta <giuseppebarbetta@outlook.com>
2024-07-04 22:54:21 -0300

    feat: create mongo and env

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
+import 'dotenv/config';
+
 import express, { json } from 'express';
 
+import { setupMongo } from './database';
 import { routes } from './routes';
 
-const port = 3333;
-const app = express();
-app.use(json());
-app.use(routes);
+setupMongo().then(() => {
+  const port = 3333;
+  const app = express();
+  app.use(json());
+  app.use(routes);
 
-app.listen(port, () => console.log(`🚀 App is running at port ${port}`));
+  app.listen(port, () => console.log(`🚀 App is running at port ${port}`));
+});
